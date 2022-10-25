8b6a8f523f08138304cc7996a27d7c3d0662bce3
Ivan Tsar <ivan.tsar99@gmail.com>
2022-10-26 02:35:19 +0300

    feature: add FileSystemVisitor project with basic methods

---
+++ FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystemVisitor
+{
+    public class FileSystemVisitor
+    {
+        private string rootDirectory = @"C:\Users\Ivan_Tsar";
+
+        public FileSystemVisitor()
+        {
+            IsDirectoryExists(rootDirectory);
+        }
+
+        public FileSystemVisitor(string rootDirectory)
+        {
+            IsDirectoryExists(rootDirectory);
+            this.rootDirectory = rootDirectory;
+        }
+
+        public string GetRootDirectory() => rootDirectory;
+
+        public IEnumerable<string> GetAllFoldersFromDirectory(string sDir)
+        {
+            foreach (var dir in Directory.GetDirectories(sDir))
+            {
+                yield return dir;
+            }
+
+            foreach (var directory in Directory.GetDirectories(sDir))
+            {
+                foreach (var dir in GetAllFilesFromDirectory(directory))
+                {
+                    yield return dir;
+                }
+            }
+        }
+
+        public IEnumerable<string> GetAllFilesFromDirectory(string sDir)
+        {
+            foreach (var file in Directory.GetFiles(sDir))
+            {
+                yield return file;
+            }
+
+            foreach (var directory in Directory.GetDirectories(sDir))
+            {
+                foreach (var file in GetAllFilesFromDirectory(directory))
+                {
+                    yield return file;
+                }
+            }
+        }
+
+        #region Private
+
+        private bool IsDirectoryExists(string dir)
+        {
+            if (Directory.Exists(dir).Equals(false))
+            {
+                throw new DirectoryNotFoundException($"Directory {rootDirectory} was not found.");
+            }
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ FileSystemVisitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileSystemVisitor
+{
+    internal class Program
+    {
+        const string root = @"C:\Users\Ivan_Tsar\Downloads";
+        static void Main(string[] args)
+        {
+            var fileVisitor = new FileSystemVisitor();
+            var files = fileVisitor.GetAllFoldersFromDirectory(root);
+
+            Console.WriteLine("List of Files");
+            foreach (var file in files)
+            {
+                Console.WriteLine("{0}", file);
+            }
+        }
+    }
+}
