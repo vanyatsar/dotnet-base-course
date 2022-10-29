37cb969caef463753c9a3cf9735379e0142bbf8a
Ivan Tsar <ivan.tsar99@gmail.com>
2022-10-29 22:12:22 +0300

    feat: add solution for the task 1

---
+++ ExceptionHandling/Task1/Exceptions/StringNullOrEmptyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Task1.Exceptions
+{
+    internal class StringNullOrEmptyException: Exception
+    {
+        public StringNullOrEmptyException(string message) : base(message)
+        {
+        }
+    }
+}
