ba7b1dd1d778af65b9f60de855f189e98438a2b6
Ivan Tsar <ivan.tsar99@gmail.com>
2022-10-30 20:48:15 +0300

    feat: add draft exceptions error handling approach

--- ExceptionHandling/Task3/UserTaskService.cs
+++ ExceptionHandling/Task3/UserTaskService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Task3.DoNotChange;
+using Task3.Exceptions;
 
 namespace Task3
 {
@@ -12,25 +13,25 @@ namespace Task3
             _userDao = userDao;
         }
 
-        public int AddTaskForUser(int userId, UserTask task)
+        public void AddTaskForUser(int userId, UserTask task)
         {
             if (userId < 0)
-                return -1;
+            {
+                throw new InvalidUserIdException($"Invalid user id: {userId}.", userId);
+            }
 
             var user = _userDao.GetUser(userId);
             if (user == null)
-                return -2;
+                throw new UserNotFoundException($"User {userId} not found.");
 
             var tasks = user.Tasks;
             foreach (var t in tasks)
             {
                 if (string.Equals(task.Description, t.Description, StringComparison.OrdinalIgnoreCase))
-                    return -3;
+                    throw new TaskAlreadyExistsException($"The task already exists.");
             }
 
             tasks.Add(task);
-
-            return 0;
         }
     }
 }
